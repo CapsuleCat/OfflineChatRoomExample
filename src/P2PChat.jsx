98856686038246cec10b6c61b42931311b479212
Ivan Montiel <idmontie@asu.edu>
2016-01-17 12:21:04 -0700

    Disconnect from Meteor, forgot to uncomment that line :sweat_smile:

--- src/P2PChat.jsx
+++ src/P2PChat.jsx
@@ -1,6 +1,6 @@
 if (Meteor.isClient) {
   Meteor.startup(function () {
-    //Meteor.disconnect();
+    Meteor.disconnect();
     ReactDOM.render(<ChatRoom />, document.getElementById('render-target'));
   });
 }
